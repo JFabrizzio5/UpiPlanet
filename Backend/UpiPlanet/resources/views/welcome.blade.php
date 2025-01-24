02c65fd0a047f3853c14441186cd25680b55de8f
jangel18 <140494868+jangel18@users.noreply.github.com>
2025-01-24 02:42:09 -0600

    Titulo

--- Backend/UpiPlanet/resources/views/welcome.blade.php
+++ Backend/UpiPlanet/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Jos5</title>
+        <title>UpiPlanet</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
