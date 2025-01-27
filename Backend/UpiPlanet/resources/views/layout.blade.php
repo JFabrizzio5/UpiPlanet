e12e762abdced19989f63ebfb3b8ac4e42f61c4a
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-01-27 01:51:50 -0600

    Ajustes en cards

--- Backend/UpiPlanet/resources/views/layout.blade.php
+++ Backend/UpiPlanet/resources/views/layout.blade.php
@@ -111,14 +111,19 @@
             </div>
         </section>
 
+        @livewireScripts
+
+        @yield('content')
+
+        @livewireStyles
 
 
 
-        <div class="px-4 py-8 mx-auto max-w-9xl">
-            <div class="flex justify-between gap-9">
+        <div class="max-w-6xl px-4 py-8 mx-auto">
+            <div class="flex justify-between gap-5">
                 <!-- Card 1 -->
                 <div
-                    class="relative w-1/3 p-8 overflow-hidden text-white shadow-2xl bg-gradient-to-r from-gray-800 to-gray-600 rounded-xl">
+                    class="relative w-1/2 p-8 overflow-hidden text-white shadow-2xl bg-gradient-to-r from-gray-800 to-gray-600 rounded-xl">
                     <div class="absolute top-0 left-0 w-full h-full transform -skew-x-12 bg-white opacity-10"></div>
                     <div class="relative z-10">
                         <svg class="w-12 h-12 mb-4 text-black" fill="currentColor" viewBox="0 0 24 24">
@@ -141,31 +146,10 @@
                 </div>
 
                 <!-- Card 2 -->
-                <div
-                    class="relative w-1/3 p-8 overflow-hidden text-white shadow-2xl bg-gradient-to-r from-gray-800 to-gray-600 rounded-xl">
-                    <div class="absolute top-0 left-0 w-full h-full transform -skew-x-12 bg-white opacity-10"></div>
-                    <div class="relative z-10">
-                        <svg class="w-12 h-12 mb-4 text-black" fill="currentColor" viewBox="0 0 24 24">
-                            <path
-                                d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
-                        </svg>
-                        <p class="mb-4 text-xl font-medium">
-                            "This is another example of a card. Modify the content here for your needs!"
-                        </p>
-                        <div class="flex items-center">
-                            <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="Avatar"
-                                class="w-12 h-12 mr-4 border-2 border-black rounded-full">
-                            <div>
-                                <h4 class="font-semibold">John Doe</h4>
-                                <p class="text-black">Software Engineer</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <!-- Card 3 -->
                 <div
-                    class="relative w-1/3 p-8 overflow-hidden text-white shadow-2xl bg-gradient-to-r from-gray-800 to-gray-600 rounded-xl">
+                    class="relative w-1/2 p-8 overflow-hidden text-white shadow-2xl bg-gradient-to-r from-gray-800 to-gray-600 rounded-xl">
                     <div class="absolute top-0 left-0 w-full h-full transform -skew-x-12 bg-white opacity-10"></div>
                     <div class="relative z-10">
                         <svg class="w-12 h-12 mb-4 text-black" fill="currentColor" viewBox="0 0 24 24">
@@ -188,11 +172,6 @@
             </div>
         </div>
 
-        @livewireScripts
-
-        @yield('content')
-
-        @livewireStyles
 
 
 
