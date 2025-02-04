de70f65df97057938cc102b1ab761e5df5652998
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-02-03 22:20:30 -0600

    2cv22

--- Backend/UpiPlanet/resources/views/livewire/horariobuscar.blade.php
+++ Backend/UpiPlanet/resources/views/livewire/horariobuscar.blade.php
@@ -18,24 +18,25 @@
         <div class="mt-3 alert alert-danger">{{ $message }}</div>
     @enderror
 
-    @if ($pdfUrl)
-        <div class="mt-3">
-            <button class="btn btn-danger" wire:click="downloadPdf">
-                <i class="fas fa-file-pdf"></i> Descargar PDF
-            </button>
+    @foreach ($capturas as $captura)
+        @dd(Storage::disk('public')->url($captura))
+        <div class="flex-shrink-0">
+            <img src="{{ Storage::disk('public')->url($captura) }}"
+                class="object-contain w-auto h-48 border-2 border-gray-300 rounded-lg" alt="Captura">
         </div>
-    @elseif ($capturas)
-        <div class="mt-3">
-            <span class="text-xl font-semibold text-gray-700">Resultados:</span>
-            <div class="flex mt-4 space-x-4 overflow-auto">
-                @foreach ($capturas as $captura)
-                    <div class="flex-shrink-0">
-                        <img src="{{ Storage::disk('public')->url($captura) }}"
-                            class="object-contain h-48 border-2 border-gray-300 rounded-lg w-72" alt="Captura">
-                    </div>
-                @endforeach
-            </div>
+    @endforeach
+
+    <div class="mt-3">
+        <span class="text-xl font-semibold text-gray-700">Resultados:</span>
+        <div class="flex mt-4 space-x-4 overflow-auto">
+            @foreach ($capturas as $captura)
+                <div class="flex-shrink-0">
+                    <img src="{{ Storage::disk('public')->url($captura) }}"
+                        class="object-contain w-auto h-48 border-2 border-gray-300 rounded-lg" alt="Captura">
+                </div>
+            @endforeach
         </div>
+    </div>
     @endif
 
     @if (empty($capturas) && !$isLoading)
