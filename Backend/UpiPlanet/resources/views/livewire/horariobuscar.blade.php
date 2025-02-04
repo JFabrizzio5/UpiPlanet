46fa92abd5ae022bc7646b378dd8f1691c5f4e4d
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-02-03 22:31:06 -0600

    Update horariobuscar.blade.php

--- Backend/UpiPlanet/resources/views/livewire/horariobuscar.blade.php
+++ Backend/UpiPlanet/resources/views/livewire/horariobuscar.blade.php
@@ -5,9 +5,9 @@
                 required>
         </div>
 
-        <button type="button"
+        <button type="button" type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            type="submit" wire:loading.attr="disabled">
+            wire:loading.attr="disabled">
             <span wire:loading.remove>Buscar y Capturar</span>
             <span wire:loading>
                 <span class="spinner-border spinner-border-sm" role="status"></span>
