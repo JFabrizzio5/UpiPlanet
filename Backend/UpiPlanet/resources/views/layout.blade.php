8aec4ac64d94f831639a8c5402dcd7e13205bbf2
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-02-10 20:50:42 -0600

    Sweet Alert

--- Backend/UpiPlanet/resources/views/layout.blade.php
+++ Backend/UpiPlanet/resources/views/layout.blade.php
@@ -14,6 +14,9 @@
 
     {{-- Flowbite --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    {{-- SweetAlert --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
 <style>
     html,
