89f7bf84179a0c72d022ca47758a801bba58387e
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-02-10 21:59:46 -0600

    Donaciones

--- Backend/UpiPlanet/resources/views/principal.blade.php
+++ Backend/UpiPlanet/resources/views/principal.blade.php
@@ -15,16 +15,16 @@
                 </p>
                 <a href="/Menu"
                     class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 dark:focus:ring-indigo-900">
-                    Inicia tu experiencias
+                    🪐 Inicia tu experiencias
                     <svg class="w-5 h-5 ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                             clip-rule="evenodd"></path>
                     </svg>
                 </a>
-                <a href="#"
+                <a href="https://link.mercadopago.com.mx/upiplanetdonacion"
                     class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Saber más
+                    🤲 Donar al proyecto
                 </a>
             </div>
             <!-- Imagen con animación de rebote -->
